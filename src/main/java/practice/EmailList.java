3725317dbe55291128cab0f3454c570d72815974
Vitaly Kshinin <vitaly.kshinin@gmail.com>
2022-11-14 16:59:52 +0300

    Homework 10.9 done

--- src/main/java/practice/EmailList.java
+++ src/main/java/practice/EmailList.java
@@ -1,19 +1,31 @@
 package practice;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import org.apache.commons.collections.list.TreeList;
+
+import java.util.*;
 
 public class EmailList {
+
+    public TreeSet<String> mail = new TreeSet<>();
+    String regexRu = "[a-zA-Z0-9]+@[a-zA-Z0-9]+[.][ru]+";
+    String regexCom = "[a-zA-Z0-9]+@[a-zA-Z0-9]+[.][com]+";
 
     public void add(String email) {
         // TODO: валидный формат email добавляется, email это строка, она быть может любой
         // принять решение добавлять аргумент email или нет должен этот метод
+        if (email.matches(regexRu) || email.matches(regexCom)) {
+            mail.add(email.toLowerCase(Locale.ROOT));
+        } else {
+            System.out.println("Неверный формат email");
+        }
     }
 
     public List<String> getSortedEmails() {
         // TODO: возвращается сортированный список электронных адресов в алфавитном порядке
-        return new ArrayList<>();
+        for (String listMail : mail) {
+            System.out.println(listMail);
+        }
+        return new TreeList(mail);
     }
 
 }
--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,3 +1,5 @@
+package practice;
+
 import java.util.Scanner;
 
 public class Main {
@@ -17,17 +19,34 @@ public class Main {
         "Неверный формат email"
     */
 
+    private static EmailList emailList = new EmailList();
+
     public static void main(String[] args) {
+        System.out.println("Введите команду \"ADD\" или \"LIST\"");
+
         Scanner scanner = new Scanner(System.in);
         
         while (true) {
-            String input = scanner.nextLine();
+            String input = scanner.nextLine().trim();
             if (input.equals("0")) {
                 break;
             }
-            
-            //TODO: write code here
-            
+
+            String[] text = input.split("\\s", 2);
+            String command = text[0].trim();
+
+            switch (command) {
+                case "ADD":
+                    String email = text[1].trim();
+                    emailList.add(email);
+                    break;
+                case "LIST":
+                    emailList.getSortedEmails();
+                    break;
+                default:
+                    System.out.println("Введена неверная команда");
+                    break;
+            }
         }
     }
 }
